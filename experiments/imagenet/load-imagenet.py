bdc1459f8e16a4b983a7999b44eb52b9de2d22b2
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2020-11-24 18:39:19 +0000

    adjusted imgnet path

--- experiments/imagenet/load-imagenet.py
+++
@@ -1,6 +0,0 @@
-import torchvision
-
-if __name__ == '__main__':
-    root_path = '/hpi/fs00/home/nils.strassenburg/datasets/imgnet'
-    imagenet_data = torchvision.datasets.ImageNet(root_path, split='val')
-    first_elem = imagenet_data[0]
