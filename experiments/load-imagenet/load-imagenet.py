7473635e1a97143c4bb7cf61d048cb148eaa03f8
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2020-11-24 11:42:40 +0000

    load and show image

--- experiments/load-imagenet/load-imagenet.py
+++ experiments/load-imagenet/load-imagenet.py
@@ -1,5 +1,6 @@
 import torchvision
 
 if __name__ == '__main__':
-    root_path = '/Users/nils/Studium/master-thesis/repo/experiments/datasets/imagenet/2012'
+    root_path = '/hpi/fs00/home/nils.strassenburg/datasets/imgnet'
     imagenet_data = torchvision.datasets.ImageNet(root_path, split='val')
+    first_elem = imagenet_data[0]
