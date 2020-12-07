19b192b54fe868e245c6d0e4a0c0d2627968792a
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2020-12-07 23:05:39 +0100

    train wip

--- experiments/helper/imagenet.py
+++ experiments/helper/imagenet.py
@@ -1,3 +1,5 @@
+import time
+
 import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms
@@ -29,7 +31,8 @@ def inference(model, dataset, batch_size, loader_workers, use_gpu=False, gpu=Non
 
     if use_gpu:
         # load model on gpu
-        model.cuda(gpu)
+        torch.cuda.set_device(gpu)
+        model = model.cuda(gpu)
 
     # set model to eval mode
     model.eval()
@@ -48,10 +51,125 @@ def inference(model, dataset, batch_size, loader_workers, use_gpu=False, gpu=Non
     return outputs
 
 
-def cpu_train(model, dataset, args):
-    data_loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers,
-                             pin_memory=True)
+def train_epoch(model, dataset, batch_size, loader_workers, loss_func, optimizer, epoch, use_gpu=False, gpu=None,
+                print_freq=1):
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=loader_workers,
+                                              pin_memory=True, )
+
+    if use_gpu:
+        # load model on gpu
+        torch.cuda.set_device(gpu)
+        model = model.cuda(gpu)
+
+    batch_time = AverageMeter('Time', ':6.3f')
+    data_time = AverageMeter('Data', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    progress = ProgressMeter(
+        len(data_loader),
+        [batch_time, data_time, losses, top1, top5],
+        prefix="Epoch: [{}]".format(epoch))
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    for i, (images, target) in enumerate(data_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        if use_gpu is not None:
+            # TODO check if blocking influences repeatability
+            images = images.cuda(gpu, non_blocking=True)
+        if torch.cuda.is_available():
+            # TODO check if blocking influences repeatability
+            target = target.cuda(gpu, non_blocking=True)
+
+        # compute output
+        output = model(images)
+        loss = loss_func(output, target)
+
+        # measure accuracy and record loss
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        losses.update(loss.item(), images.size(0))
+        top1.update(acc1[0], images.size(0))
+        top5.update(acc5[0], images.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % print_freq == 0:
+            progress.display(i)
 
 
-def gpu_train():
-    pass
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+
+def adjust_learning_rate(optimizer, epoch, args):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = args.lr * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
