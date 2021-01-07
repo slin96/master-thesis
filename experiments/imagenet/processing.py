29eafc2d709b6e65b14a6734d6df0e196c65862b
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-01-07 15:24:39 +0000

    formatting

--- experiments/imagenet/processing.py
+++ experiments/imagenet/processing.py
@@ -13,79 +13,9 @@ from experiments.repeatability.util import get_device
 # Explanation for magic numbers: https://github.com/pytorch/vision/pull/1965
 
 
-def train_epoch(model, data, loss_func, optimizer, epoch=0, batch_size=64, num_workers=1, device=None, print_freq=1,
-                get_outputs=False, number_batches=None):
-    device = get_device(device)
-
-    # TODO check params for reproducibility -> num workers
-    train_loader = torch.utils.data.DataLoader(data, batch_size=batch_size, shuffle=True, num_workers=num_workers,
-                                               pin_memory=True)
-
-    model.to(device)
-
-    batch_time = AverageMeter('Time', ':6.3f')
-    data_time = AverageMeter('Data', ':6.3f')
-    losses = AverageMeter('Loss', ':.4e')
-    top1 = AverageMeter('Acc@1', ':6.2f')
-    top5 = AverageMeter('Acc@5', ':6.2f')
-    progress = ProgressMeter(
-        len(train_loader),
-        [batch_time, data_time, losses, top1, top5],
-        prefix="Epoch: [{}]".format(epoch))
-
-    # switch to train mode
-    model.train()
-
-    outputs = []
-
-    end = time.time()
-    for i, (images, target) in enumerate(train_loader):
-        # measure data loading time
-        data_time.update(time.time() - end)
-
-        images = images.to(device)
-        target = target.to(device)
-
-        # compute output
-        output = model(images)
-        loss = loss_func(output, target)
-
-        # measure accuracy and record loss
-        acc_record_loss(images, loss, losses, output, target, top1, top5)
-
-        # compute gradient and do SGD step
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        # measure elapsed time
-        batch_time.update(time.time() - end)
-        end = time.time()
-
-        if i % print_freq == 0:
-            progress.display(i)
-
-        if get_outputs:
-            outputs.append(output)
-
-        if not in_number_of_batches(i, number_batches):
-            break
-
-    if get_outputs:
-        return output
-
-
-def acc_record_loss(images, loss, losses, output, target, top1, top5):
-    acc1, acc5 = accuracy(output, target, topk=(1, 5))
-    losses.update(loss.item(), images.size(0))
-    top1.update(acc1[0], images.size(0))
-    top5.update(acc5[0], images.size(0))
-
-
-def validate(model, data, loss_func, batch_size=64, num_workers=1, device=None, print_freq=1, get_outputs=False,
+def validate(model, data, loss_func, batch_size=64, num_workers=0, device=None, print_freq=1, get_outputs=False,
              number_batches=None):
     device = get_device(device)
-    # TODO check params for reproducibility -> num workers
 
     val_loader = torch.utils.data.DataLoader(data, batch_size=batch_size, shuffle=False, num_workers=num_workers,
                                              pin_memory=True)
@@ -142,5 +72,71 @@ def validate(model, data, loss_func, batch_size=64, num_workers=1, device=None, 
         return top1.avg
 
 
+def train_epoch(model, data, loss_func, optimizer, epoch=0, batch_size=64, num_workers=0, device=None, print_freq=1,
+                get_outputs=False, number_batches=None):
+    device = get_device(device)
+    train_loader = torch.utils.data.DataLoader(data, batch_size=batch_size, shuffle=True, num_workers=num_workers,
+                                               pin_memory=True)
+
+    model.to(device)
+
+    batch_time = AverageMeter('Time', ':6.3f')
+    data_time = AverageMeter('Data', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    progress = ProgressMeter(
+        len(train_loader),
+        [batch_time, data_time, losses, top1, top5],
+        prefix="Epoch: [{}]".format(epoch))
+
+    # switch to train mode
+    model.train()
+
+    outputs = []
+
+    end = time.time()
+    for i, (images, target) in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        images = images.to(device)
+        target = target.to(device)
+
+        # compute output
+        output = model(images)
+        loss = loss_func(output, target)
+
+        # measure accuracy and record loss
+        acc_record_loss(images, loss, losses, output, target, top1, top5)
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % print_freq == 0:
+            progress.display(i)
+
+        if get_outputs:
+            outputs.append(output)
+
+        if not in_number_of_batches(i, number_batches):
+            break
+
+    if get_outputs:
+        return output
+
+
 def in_number_of_batches(i, number_batches):
     return number_batches is None or i < number_batches - 1
+
+
+def acc_record_loss(images, loss, losses, output, target, top1, top5):
+    acc1, acc5 = accuracy(output, target, topk=(1, 5))
+    losses.update(loss.item(), images.size(0))
+    top1.update(acc1[0], images.size(0))
+    top5.update(acc5[0], images.size(0))
