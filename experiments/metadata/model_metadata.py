1cc1ac0d3f7638407338ff6aa9aca97f005ef83d
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-01-08 10:49:50 +0000

    added models to model metadata

--- experiments/metadata/model_metadata.py
+++ experiments/metadata/model_metadata.py
@@ -2,7 +2,8 @@ from torchsummary import summary
 from torchvision import models
 
 if __name__ == '__main__':
-    models = [models.alexnet, models.vgg19, models.resnet18, models.resnet50, models.resnet152]
+    models = [models.alexnet, models.vgg19, models.shufflenet_v2_x0_5, models.mobilenet_v2, models.googlenet,
+              models.resnet18, models.resnet50, models.resnet152]
 
     for mod in models:
         model = mod()
