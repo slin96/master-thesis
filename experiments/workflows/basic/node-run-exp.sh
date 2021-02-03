50b77fe4213eba20fd3d7aec67c60628e7bd95d9
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-02-03 11:11:34 +0100

    rename u1 to basic

---
+++ experiments/workflows/basic/node-run-exp.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+while getopts a: flag
+do
+    case "${flag}" in
+        a) APPROACH=${OPTARG};;
+    esac
+done
+
+
+echo "used model: $MODEL";
+
+# mandatory arguments
+if [ ! "$APPROACH" ]; then
+  echo "argument -a must be provided to specify the approach"
+  exit 1
+fi
+
+cd "$(dirname "$0")"
+
+python /node/experiments/workflows/basic/approaches/$APPROACH/node.py --tmp_dir /shared --mongo_ip mongo-db --server_ip server-container --node_ip node-container --log_dir /shared/logs/$APPROACH > /shared/logs/$APPROACH/python-node.log
--- experiments/workflows/basic/server-run-exp.sh
+++ experiments/workflows/basic/server-run-exp.sh
@@ -27,4 +27,4 @@ cd "$(dirname "$0")"
 # wait for node to be ready for listening
 sleep 3
 
-python /server/experiments/workflows/u1/approaches/$APPROACH/server.py --model $MODEL_NAME --tmp_dir /shared --model_code /server/experiments/models/$MODEL_CODE --import_root /server --mongo_ip mongo-db --server_ip server-container --node_ip node-container --log_dir /shared/logs/$APPROACH > /shared/logs/$APPROACH/python-server.log
+python /server/experiments/workflows/basic/approaches/$APPROACH/server.py --model $MODEL_NAME --tmp_dir /shared --model_code /server/experiments/models/$MODEL_CODE --import_root /server --mongo_ip mongo-db --server_ip server-container --node_ip node-container --log_dir /shared/logs/$APPROACH > /shared/logs/$APPROACH/python-server.log
