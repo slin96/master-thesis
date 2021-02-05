3a231a4287d460036c2f31ff48a33617d0ae0735
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-02-05 10:34:11 +0100

    measure time for server

--- experiments/workflows/basic/approaches/baseline/server.py
+++ experiments/workflows/basic/approaches/baseline/server.py
@@ -4,6 +4,7 @@ from time import sleep
 from mmlib.log import use_model
 from mmlib.save import FileSystemMongoSaveRecoverService
 
+from experiments.measure.eventtimer import EventTimer
 from experiments.workflows.server_shared import *
 from experiments.workflows.shared import *
 # to run this make sure mongoDB is running:
@@ -12,6 +13,7 @@ from experiments.workflows.shared import generate_message, inform
 
 SERVER = 'server'
 
+server_timer = EventTimer()
 
 def main(args):
     save_service = FileSystemMongoSaveRecoverService(args.tmp_dir, args.mongo_ip)
@@ -19,8 +21,14 @@ def main(args):
     print('model used: {}'.format(args.model))
     # initially train the model in full dataset
     init_model = initial_train(models_dict[args.model])
+
+    # time save the initial model
+    time_name = 'save-initial'
+    server_timer.start_event(time_name)
     # save the initially trained model
     init_model_id = save_service.save_model(args.model, init_model, args.model_code, args.import_root)
+    server_timer.stop_event(time_name)
+    # -------------------------------------
 
     # NOT TIMED: save state_dict and output to compare restored model
     save_compare_info(init_model, SERVER, init_model_id, args.log_dir)
@@ -38,7 +46,13 @@ def main(args):
     updated_model = update_model(init_model)
     # save the updated model
     updated_model_name = args.model + '-updated'
+
+    # time save the updated model
+    time_name = 'save-updated'
+    server_timer.start_event(time_name)
     updated_model_id = save_service.save_model(updated_model_name, updated_model, args.model_code, args.import_root)
+    server_timer.stop_event(time_name)
+    # -------------------------------------
 
     # NOT TIMED: save state_dict and output to compare restored model
     save_compare_info(updated_model, SERVER, updated_model_id, args.log_dir)
@@ -59,7 +73,14 @@ def react_to_new_model(msg):
 
     # as soon as new model is available
     save_recover_service = FileSystemMongoSaveRecoverService(args.tmp_dir, args.mongo_ip)
+
+    # time save the updated model
+    time_name = 'recover-model'
+    server_timer.start_event(time_name)
     recovered_model = save_recover_service.recover_model(model_id)
+    server_timer.stop_event(time_name)
+    # -------------------------------------
+
     # use recovered model
     use_model(model_id)
 
@@ -71,6 +92,8 @@ def react_to_new_model(msg):
         listen((args.node_ip, args.node_port), react_to_new_model)
     else:
         print('Server done')
+        print(server_timer.get_time_line())
+        print(server_timer.get_elapsed_times())
 
 
 def parse_args():
