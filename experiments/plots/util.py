15df298a6265122cbd8902760a96cace107afd6c
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-06-12 15:45:55 +0200

    parse single event

--- experiments/plots/util.py
+++ experiments/plots/util.py
@@ -1,5 +1,13 @@
 import json
 
+STOP = 'stop'
+
+ID = '_id'
+
+START = 'start'
+
+START_STOP = 'start-stop'
+
 EVENT = 'event'
 
 
@@ -72,3 +80,61 @@ def create_pie_chart_sizes_and_labels(storage_distribution):
         sizes.append(size / total_size * 100)
 
     return labels, sizes
+
+
+class Event:
+
+    def __init__(self, start_json, stop_json, children):
+        self.start_json = start_json
+        self.stop_json = stop_json
+        self.children = children
+
+
+def parse_events(file):
+    with open(file) as f:
+        lines = f.readlines()
+
+    filtered_lines = filter_non_events(lines)
+    events = _parse_event(filtered_lines)
+    return events
+
+
+def filter_non_events(lines):
+    return list(filter(lambda l: START_STOP in l, lines))
+
+
+def _parse_event(lines: list, _continue=True):
+    start_line = lines.pop(0)
+    start_line_json = json.loads(start_line)
+    assert start_line_json[START_STOP].lower() == START
+    start_id = start_line_json[ID]
+    print(start_line)
+
+    next_line = lines[0]
+    next_line_json = json.loads(next_line)
+    next_id = next_line_json[ID]
+
+    if start_id == next_id:
+        assert next_line_json[START_STOP].lower() == STOP
+        # remove stop line so that next line for parsing is start again
+        lines.pop(0)
+        e = Event(start_line_json, next_line_json, [])
+        return e
+    else:
+        assert next_line_json[START_STOP].lower() == START
+        children_events = []
+        while not next_id == start_id:
+            child_event = _parse_event(lines, _continue=False)
+            children_events.append(child_event)
+            next_line = lines[0]
+            next_line_json = json.loads(next_line)
+            next_id = next_line_json[ID]
+        assert next_id == start_id
+        assert next_line_json[START_STOP].lower() == STOP
+        e = Event(start_line_json, next_line_json, children_events)
+        lines.pop(0)
+        return e
+
+
+if __name__ == '__main__':
+    parse_events('/Users/nils/Studium/master-thesis/repo/tmp/res/server-baseline-out.txt')
