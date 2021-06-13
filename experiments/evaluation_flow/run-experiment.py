0885a0844800833775e1d508dbc46cf55c04824e
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-06-13 11:48:50 +0200

    restructure commands

--- experiments/evaluation_flow/run-experiment.py
+++ experiments/evaluation_flow/run-experiment.py
@@ -2,7 +2,6 @@ import argparse
 import os
 import pipes
 import subprocess
-import time
 
 
 def exists_remote(host, path):
@@ -17,11 +16,52 @@ def exists_remote(host, path):
     raise Exception('SSH failed')
 
 
+def execute_commands(hostname, commands, to_background=False):
+    joint_commands = ';'.join(commands)
+    cmd = "ssh -t {} '{}'".format(hostname, joint_commands)
+    if to_background:
+        cmd += ' &'
+
+    os.system(cmd)
+
+
 def main(args):
-    # os.system("ssh -t dlab-n06 'conda activate myenv;"
-    #           "export PYTHONPATH=\"/hpi/fs00/home/nils.strassenburg/evaluation/node/\";"
-    #           "cd /hpi/fs00/home/nils.strassenburg/evaluation/node/experiments/evaluation_flow;"
-    #           "python node.py --tmp_dir /hpi/fs00/home/nils.strassenburg/tmp-dir --server_ip 172.20.26.34 --node_ip 172.20.26.35 --mongo_host 172.20.26.33 --model resnet152 --approach param_update --model_snapshots /hpi/fs00/share/fg-rabl/strassenburg/version-snapshots/resnet152-versions-outdoor --snapshot_type fine-tuned --u3_count 3 > remote-test.txt'")
+    activate_env = "conda activate {}".format(args.env_name)
+
+    set_node_python_path = "export PYTHONPATH=\"{}\"".format(args.node_pythonpath)
+    set_server_python_path = "export PYTHONPATH=\"{}\"".format(args.server_pythonpath)
+
+    cd_to_node_script = "cd {}".format(args.node_script_root)
+    cd_to_server_script = "cd {}".format(args.server_script_root)
+
+    tmp_dir_arg = "--tmp_dir {}".format("/hpi/fs00/home/nils.strassenburg/tmp-dir")
+    server_ip_arg = "--server_ip {}".format('172.20.26.34')
+    node_ip_arg = "--node_ip {}".format('172.20.26.35')
+    mongo_host_arg = "--mongo_host {}".format('172.20.26.33')
+    model_arg = "--model {}".format('mobilenet')
+    approach_arg = "--approach {}".format('param_update')
+    model_snapshot_args = "--model_snapshots {}".format(
+        '/hpi/fs00/share/fg-rabl/strassenburg/version-snapshots/mobilenet-versions-outdoor')
+    snapshot_type_arg = "--snapshot_type {}".format('fine-tuned')
+    u3_count_arg = "--u3_count {}".format('3')
+
+    node_out_file = "> {}".format('node-remote-test.txt')
+    server_out_file = "> {}".format('server-remote-test.txt')
+
+    node_parameters = [tmp_dir_arg, node_ip_arg, server_ip_arg, mongo_host_arg, model_arg, approach_arg,
+                       model_snapshot_args, snapshot_type_arg, u3_count_arg, node_out_file]
+    run_node_cmd = "python node.py {}".format(" ".join(node_parameters))
+
+    server_parameters = [tmp_dir_arg, server_ip_arg, node_ip_arg, mongo_host_arg, model_arg, approach_arg,
+                         model_snapshot_args, snapshot_type_arg, server_out_file]
+    run_server_cmd = "python server.py {}".format(" ".join(server_parameters))
+
+    # run node
+    execute_commands('dlab-n06', [activate_env, set_node_python_path, cd_to_node_script, run_node_cmd, node_out_file],
+                     to_background=True)
+    # run server
+    execute_commands('dlab-n05', [activate_env, set_server_python_path, cd_to_server_script, run_server_cmd],
+                     to_background=True)
 
     # cmd = ''
     # cmd += "ssh -t {} 'conda activate {};".format(args.host_name, args.env_name)
@@ -39,26 +79,30 @@ def main(args):
     # cmd += ' &'
     # os.system(cmd)
 
-    result = exists_remote('dlab-n05',
-                           '/hpi/fs00/home/nils.strassenburg/evaluation/server/experiments/evaluation_flow/done.txt')
-    print(result)
 
-    # start mongo
-    os.system("ssh -t {} '{};{}' &".format('dlab-n04', 'export XDG_DATA_HOME=/scratch/$(id -un)/enroot', 'enroot start mongo'))
-
-    time.sleep(20)
-
-    # kill mongoDB
-    os.system('ssh -t {} pkill -f mongo'.format('dlab-n04'))
-
+# result = exists_remote('dlab-n05',
+#                        '/hpi/fs00/home/nils.strassenburg/evaluation/server/experiments/evaluation_flow/done.txt')
+# print(result)
+#
+# # start mongo
+# os.system("ssh -t {} '{};{}' &".format('dlab-n04', 'export XDG_DATA_HOME=/scratch/$(id -un)/enroot',
+#                                        'enroot start mongo'))
+#
+# time.sleep(20)
+#
+# # kill mongoDB
+# os.system('ssh -t {} pkill -f mongo'.format('dlab-n04'))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--host-name', type=str, default='dlab-n06')
     parser.add_argument('--env-name', type=str, default='myenv')
-    parser.add_argument('--pythonpath', type=str, default='/hpi/fs00/home/nils.strassenburg/evaluation/node/')
-    parser.add_argument('--script-root', type=str,
+    parser.add_argument('--node-pythonpath', type=str, default='/hpi/fs00/home/nils.strassenburg/evaluation/node/')
+    parser.add_argument('--server-pythonpath', type=str, default='/hpi/fs00/home/nils.strassenburg/evaluation/server/')
+    parser.add_argument('--node-script-root', type=str,
                         default='/hpi/fs00/home/nils.strassenburg/evaluation/node/experiments/evaluation_flow')
+    parser.add_argument('--server-script-root', type=str,
+                        default='/hpi/fs00/home/nils.strassenburg/evaluation/server/experiments/evaluation_flow')
 
     args = parser.parse_args()
     main(args)
