2c40f263d3fb928435cadf8aa8a3f13ab34f1498
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-06-22 17:00:18 +0200

    median train time

--- experiments/snapshot_training_times/util.py
+++ experiments/snapshot_training_times/util.py
@@ -1,15 +1,6 @@
 import os
 
-RESNET_152 = 'resnet152'
-RESNET_50 = 'resnet50'
-RESNET_18 = 'resnet18'
-GOOGLENET = 'googlenet'
-MOBILENET = 'mobilenet'
-U2_LOGS = './snapshot-training-logs/u2'
-U3_FOOD_LOGS = './snapshot-training-logs/u3-1-food'
-U2_OUTDOOR_LOGS = './snapshot-training-logs/u3-1-outdoor'
 FILENAME_TEMPLATE = '{}-out.txt'
-MODELS = [MOBILENET, GOOGLENET, RESNET_18, RESNET_50, RESNET_152]
 
 
 def train_times(model, log_dir):
@@ -29,6 +20,10 @@ def train_times(model, log_dir):
             training_times[epoch_number]['stop'] = time_ns
             training_times[epoch_number]['diff'] = time_ns - training_times[epoch_number]['start']
 
+    for k in list(training_times.keys()):
+        if 'diff' not in training_times[k]:
+            del training_times[k]
+
     return training_times
 
 
@@ -38,9 +33,3 @@ def get_epoch_number(line):
 
 def get_time_ns(line):
     return int(line.split(';')[4].replace('time.time_ns-', ''))
-
-
-if __name__ == '__main__':
-    googlenet_u2_times = train_times('googlenet', U2_LOGS)
-    googlenet_u3_times = train_times('googlenet', U3_FOOD_LOGS)
-    print('test')
